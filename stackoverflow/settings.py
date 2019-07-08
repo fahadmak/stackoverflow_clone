9f6fa7cdcb3d32bff46324385bf6dec2ae29e400
fahadmak <fahad.makabugo@andela.com>
2019-07-08 17:01:43 +0300

    Chore(coveralls, TravisCi): Integrate TravisCi and Coveralls with README badges
    - Set up TravisCi and coveralls
    - Add repo_token to travis environment variable online
    - Add coveralls and travis badge to Readme.md file
    - Create .travis.yml file
    
    [Maintains #167133886]

--- stackoverflow/settings.py
+++ stackoverflow/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -75,11 +77,25 @@ WSGI_APPLICATION = 'stackoverflow.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
