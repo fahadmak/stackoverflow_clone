067ed5f8d2033beae63c9b6795ff9162dc98bfbe
fahadmak <fahad.makabugo@andela.com>
2019-07-10 17:38:55 +0300

    feature(Questions): Create Questions Functionality
    - create more tests for for QuestionView
    - Add generic ListView in the QuestionView
    - Add question card in question/index.html template
    
    [Delivers #167137965]

--- questions/views.py
+++ questions/views.py
@@ -1,19 +1,10 @@
-from django.shortcuts import render, redirect
-from django.views.generic import TemplateView
-from django.urls import reverse
-from questions.forms import CreateQuestionForm
+from django.views.generic import ListView, CreateView
+from .forms import CreateQuestionForm
+from .models import Question
 
 
-class QuestionView(TemplateView):
+class QuestionView(CreateView, ListView):
+    form_class = CreateQuestionForm
+    success_url = '/'
     template_name = 'questions/index.html'
-
-    def post(self, request):
-        form = CreateQuestionForm(request.POST)
-        if form.is_valid():
-            form.save()
-            form = CreateQuestionForm()
-            return redirect(reverse("questions:question_list"))
-        context = {
-            'form': form,
-        }
-        return render(request, self.template_name, context)
+    model = Question
