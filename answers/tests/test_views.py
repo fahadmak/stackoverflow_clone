1cc9eda522e2451f3d1296a7808d781970f96fc3
fahadmak <fahad.makabugo@andela.com>
2019-07-17 08:41:14 +0300

    feature(Comment): Create and List Comments for a question
    - create CreateQuestionCommentForm Form
    - create QuestionComment model
    - create form in question_detail.html template
    
    [Delivers #167138181]

--- answers/tests/test_views.py
+++ answers/tests/test_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
-from questions.models import Question
+from questions.models import Question, QuestionComment
 from accounts.models import User
 from answers.models import Answer
 
@@ -27,6 +27,13 @@ class TestQuestionView(TestCase):
                 question=self.question
             )
 
+        for author_id in range(number_of_answers):
+            QuestionComment.objects.create(
+                comment=f'Christian {author_id}',
+                author=user,
+                question=self.question
+            )
+
     def test_answer_created(self):
         # send login data
         response = self.client.post('/accounts/login/', self.credentials, follow=True)
@@ -37,10 +44,29 @@ class TestQuestionView(TestCase):
                                      {'title': 'titles'})
         self.assertEqual(response.status_code, 200)
 
-    def test_lists_all_questions(self):
+    def test_lists_all_answers(self):
         # Get second page and confirm it has (exactly) remaining 3 items
         self.client.post('/accounts/login/', self.credentials, follow=True)
         response = self.client.get(reverse('answers:answer_list', kwargs={'pk': self.question.id}))
         print(self.question.id)
         self.assertEqual(response.status_code, 200)
         self.assertTrue(len(response.context['answer_list']) == 13)
+
+    def test_comment_created(self):
+        # send login data
+        response = self.client.post('/accounts/login/', self.credentials, follow=True)
+        # should be logged in now
+        self.assertTrue(response.context['user'].is_active)
+
+        response3 = self.client.post(reverse('answers:answer_list', kwargs={'pk': self.question.id}),
+                                     {'comment': 'titles'})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response3, 'questions/question_detail.html')
+
+
+    def test_lists_all_comments(self):
+        # Get second page and confirm it has (exactly) remaining 3 items
+        self.client.post('/accounts/login/', self.credentials, follow=True)
+        response = self.client.get(reverse('answers:answer_list', kwargs={'pk': self.question.id}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(len(response.context['question_comments']) == 13)
